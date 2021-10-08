ef49c72af4f120a4f73457a1dbc2b86a25ea35be
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-10-07 21:10:04 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+let monto: number = 400;
+let cantidad: number = 2;
+let montoConDescuento: number;
+let descuento: number;
+let montoSinDescuento: number;
+montoSinDescuento = monto * cantidad;
+descuento = monto * 0.1;
+montoConDescuento = monto - descuento;
+if (monto >= 1000) {
+ console.log("el precio es "montoConDescuento);
+}else{
+      console.log("el precio es" montoSinDescuento);
+    }
+      
+
+
+
+  
+    
+    
+
+
+
+
